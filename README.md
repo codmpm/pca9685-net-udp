bb0cbff6e42db62d6599574b3d0d3ae804d5dd7d
Patrik Mayer <patrik.mayer@codm.de>
2017-01-26 22:16:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Arduino PCA9685 UDP Dimmer
+
+
+A simple approach to operate the NXP [PCA9685 16-channel 12-bit PWM i2c LED controller](https://cdn-shop.adafruit.com/datasheets/PCA9685.pdf)
+via network using an Arduino and an Arduino Ethernet-Shield. The LED controller used is the [Adafruit 16-Channel 12-bit PWM/Servo Driver](https://www.adafruit.com/product/815) for which Adafruit provides a very good [Arduino Library](https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library).
+
+## Use Case
+The idea is to control each channel separately from an automation software via UDP. One of the main benefits over Hardware PWM from Arduino's ATMega is the resolution of the PCA9685 which is 12-bit in comparison to 8-bit. So you don't have 0-255 brightness levels but 0-4095! 
+So you have better dimming control on lower brightnesses.
+
+![PCA9685 12-bit LEDs](adafruit-pca9685-leds.jpg)
+(Well, not really visible in the picture...)
+
+If you wan't to drive (e.g.) a RGB-Stripe you will be limited to 0-255 for a channel, as every color picker output the color values from 0-255. In that case, simply multiply the output by 16. Of course you will loose the smoothness of 12-bit. 
+Please keep in mind, that the LED Controller itself only supplies 10-25mA for each channel. So logically you have to use a Transistor or a MOSFET drive the load. See page 29 of the chip's datasheet.
+
+## Setup and Software
+Simply wire the LED Controller to the i2c bus of your Arduino and configure the wanted UDP Port and if you want the output of the PWM be inverted or not. The code is doint DHCP with the given MAC address. If you need static IP, it should be easy to modify according to the Arduino Ethernet Examples.
+
+Don't forget to install [Adafruits PCA9685 Arduino Library](https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library).
+
+I am controlling the device from [node-red](https://nodered.org/) with [node-red-dashboard](https://github.com/node-red/node-red-dashboard), but any UDP connection will work. So any home automation software that can output UDP, like Loxone, do the job. 
+
+Just send a text string in the given format: 
+
+    <channel-number>;<brightness>
+
+So, for example `3;2048` will dimm channel 4 to 50%. Channel number and brightness values range from `0-15` and `0-4095`.
+
+![Arduino Setup and Wiring](arduino-pca9685-setup.jpg)
+
+## History
+I stumbled over a project to controll a DMX Dimmer via the [Loxone Home Automation System](https://www.loxone.com/) over UDP: https://sourceforge.net/projects/udptodmx/. 
+This inspired me to do the same with the PCA9685 which I had laying around from another project. The handling of UDP messages is vastly migrated from Robert L.'s work.
+
+---
+Created with Arduino 1.8.1 using an Freaduino Uno and an Arduino W5100 Ethernet Shield.
+
+
+
---
+++ pca9685-net-udp/pca9685-net-udp.ino
@@ -0,0 +1,139 @@
+/*
+Copyright (c) 2017 Patrik Mayer - patrik.mayer@codm.de
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+ 
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <Adafruit_PWMServoDriver.h>
+
+EthernetClient ethClient;
+EthernetUDP udp;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+
+//-------- config --------
+#define udpPort 7000
+#define pwmInvert true //should the pwm outputs be inverted?
+
+byte mac[] = {
+  0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF
+};
+
+//------------------------
+
+#define msgMaxSize 16
+#define channelCount 16
+
+char msgBuffer[msgMaxSize];
+unsigned int channelValues[channelCount];
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("starting up...");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for (;;)
+      ;
+  }
+
+  // print your local IP address:
+  printIPAddress();
+
+  pwm.begin();
+  pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
+
+  //init pca9685 all outputs to 0%
+  for (uint8_t i = 0; i < channelCount; i++) {
+    pwm.setPin(i, 0, pwmInvert);
+    channelValues[i] = 0;
+  }
+
+  //bring up the udp listener
+  udp.begin(udpPort);
+
+  //ready, blink LED_BUILTIN
+  for (uint8_t i = 0; i < 4; i++) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(140);
+  }
+
+  Serial.println("ready...");
+
+
+}
+
+void loop() {
+
+  //maintain the ethernet connection
+  Ethernet.maintain();
+
+  //handle incoming UDP messages and set channels accordingly
+  handleUdp();
+
+}
+
+void handleUdp() {
+
+  if (udp.parsePacket())  {
+    udp.read(msgBuffer, msgMaxSize);
+
+    //Serial.println(msgBuffer);
+    handleMsg();
+
+    //clear packet buffer
+    memset(msgBuffer, 0, sizeof msgBuffer);
+
+  }
+}
+
+void handleMsg() {
+  String strBuffer(msgBuffer);
+
+  uint8_t delim = strBuffer.indexOf(";");
+  uint8_t ch = strBuffer.substring(0, delim).toInt();
+  uint16_t val = strBuffer.substring(delim + 1).toInt();
+
+  ch = (ch < 16) ? ch : 0;
+  val = (val < 4096) ? val : 4095;  
+
+  //set desired value for desired channel
+  pwm.setPin(ch, val, pwmInvert);
+}
+
+void printIPAddress() {
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+
+  Serial.println();
+}
+
